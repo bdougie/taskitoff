e580a82e0a67d0e8a060d7956f16fd4efa9b4447
Brianllamar <ilikerobot@gmail.com>
2014-01-15 22:29:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskitoff::Application.config.secret_token = '8e1c02369594f5cb96ccd0eff13682e4df732e6402c4bce5f966776f44f00119869c682d2fdc26f2747c511d2311b48a0e68f74653bf0adf62e7853cf0be9baf'
