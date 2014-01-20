e59e18f910ab5dc0428f058f9d4c86a8ffe9b776
Brianllamar <ilikerobot@gmail.com>
2014-01-19 20:53:06 -0500

    updated views to show and edit todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,8 +8,31 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
   end
 
   def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def create
+    @todo = Todo.new(params[:id])
+    if @todo.save
+      redirect_to todos_path, notice: "Todo was saved successfully. Now get to work!"
+    else
+      flash[:error] = "Sorry bud, but there was an error. Please try again"
+      render :new
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+   if @topic.destroy
+      flash[:notice] = "\"#{description}\"[0..6] was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
+    end
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,8 @@
 class Todo < ActiveRecord::Base
-attr_accessible :description
+attr_accessible :description, :name
+
+belongs_to :user
 
 	validates :description, length: { maximum: 20 }
+	
 end
