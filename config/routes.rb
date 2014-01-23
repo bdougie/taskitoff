a853027ed07f82150a1bf269dc7572ea6c0a42cb
Brianllamar <ilikerobot@gmail.com>
2014-01-23 06:42:22 -0500

    working on getting the completed button to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Taskitoff::Application.routes.draw do
 
  devise_for :users
 
- resources :todos
+	 resources :todos do
+
+	  post 'completed', to: 'todos#completed'
+	 
+	 end
 
   root :to => 'todos#index' #rails wont except root 'pages#home'
 
-  get "no" => "pages#no"
+
   
 end
